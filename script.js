8bb0e7f40b6fea3d9df773382be9547635bd022a
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-15 15:00:00 +0200

    add function generateDivs()
    not working yet

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateDivs() {
+    let fragment = new DocumentFragment();  //create a virtual DOM element, 
+    //make all of DOM changes on it, and then assign that virtual DOM element 
+    //to real DOM once they are ready to commit
+
+    for (let i = 1; i <= 16; i++) {
+        let divElement = document.createElement("div");
+        divElement.classList.add("item");
+        fragment.appendChild(divElement);
+    }
+    document.getElementById("container").appendChild(fragment);
+}
