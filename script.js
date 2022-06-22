f6c316391130312a7b6387170ca36417d8076ab9
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-22 17:09:07 +0200

    change amount of created divs to 256

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function generateDivs() {
     //make all of DOM changes on it, and then assign that virtual DOM element 
     //to real DOM once they are ready to commit
 
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= 256; i++) {
         let divElement = document.createElement("div");
         divElement.classList.add("item");
         divElement.id = `item${i}`;
