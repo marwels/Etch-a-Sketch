3f515b3738b4e97eb875a320e2b2a5154e914800
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-23 14:29:52 +0200

    change newCanva()
    now it chackes if prompt is a number from 1 to 100

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function generateDivs() {
+function newCanva() {
+    let size = parseInt(prompt("Please enter size of new page. It has to be number from 1 to 100:", "50"), 10);
+
+    if (size < 1 || size > 100) {
+        parseInt(prompt("Please enter a number from 1 to 100", "50"), 10);
+    } else if (isNaN(size)) {
+        parseInt(prompt("It is not a number. Please enter a number from 1 to 100", "50"), 10);
+    }
+
+    generateDivs(size);
+}
+
+function generateDivs(size) {
     let fragment = new DocumentFragment();  //create a virtual DOM element, 
     //make all of DOM changes on it, and then assign that virtual DOM element 
     //to real DOM once they are ready to commit
 
-    for (let i = 1; i <= 256; i++) {
+    for (let i = 1; i <= size; i++) {
         let divElement = document.createElement("div");
         divElement.classList.add("item");
         divElement.id = `item${i}`;
@@ -19,8 +31,3 @@ document.querySelectorAll(".item").forEach(item => {
         item.classList.add("black")
     })
 })
-
-function newCanva() {
-    let size = prompt("Please enter size of new page. It has to be number from 1 to 100:", "50");
-    return size;
-}
